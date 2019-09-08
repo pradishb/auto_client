5fe531878452bb3974cd625a2fa0b57aad579e90
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-09-08 20:32:07 +0545

    Adds leaverbuster warning and login session

--- auto_client/status/status.py
+++ auto_client/status/status.py
@@ -2,44 +2,107 @@
 from requests import get
 from requests.exceptions import RequestException
 
-def is_client_connected(connection):
+BUGGED_DESCRIPTION = (
+    'RSO Server error: '
+    'Error response for POST /lol-rso-auth/v1/authorization/gas: '
+    'server_error: Received invalid JSON'
+)
+
+def is_client_connected(connection, *_):
     ''' Returns if client is connected '''
     if connection.url is None:
-        return False
-    return True
+        return []
+    return ['client_connected']
 
-def is_lcu_connected(connection):
+def is_lcu_connected(connection, status):
     ''' Returns if lcu is connected '''
+    if 'client_connected' not in status:
+        return []
     if connection.url is None:
-        return False
+        return []
     try:
         res = get(connection.url + '/lol-service-status/v1/lcu-status', **connection.kwargs)
     except RequestException:
-        return False
+        return []
     if res.status_code == 404:
-        return False
+        return []
     res_json = res.json()
     try:
         if res_json['status'] == 'online':
-            return True
+            return ['lcu_connected']
     except KeyError:
         pass
-    return False
+    return []
 
-STATUS_MAPPING = {
-    'client_connected': is_client_connected,
-    'lcu_connected': is_lcu_connected,
-}
+def is_leaverbuster_warning(connection, status):
+    ''' Returns if leaverbuster warning exists '''
+    if 'lcu_connected' not in status:
+        return []
+    if connection.url is None:
+        return []
+    try:
+        res = get(connection.url + '/lol-leaver-buster/v1/notifications/', **connection.kwargs)
+    except RequestException:
+        return []
+    res_json = res.json()
+    for notification in res_json:
+        if notification['type'] == 'TaintedWarning':
+            return 'leaverbuster_warning'
+    return []
+
+def check_login_session(connection, status):
+    ''' Checks login session and returns status '''
+    if 'lcu_connected' not in status:
+        return []
+    if connection.url is None:
+        return []
+    try:
+        res = get(connection.url + '/lol-login/v1/session/', **connection.kwargs)
+    except RequestException:
+        return []
+    res_json = res.json()
+    output = []
+    if 'state' in res_json:
+        if res_json['isNewPlayer']:
+            output.append('new_player')
+        if res_json['state'] == 'SUCCEEDED':
+            output.append('login_succeed')
+        if res_json['state'] == 'ERROR':
+            if res_json['error']['messageId'] == 'ACCOUNT_BANNED':
+                output.append('banned')
+            if res_json['error']['description'] == BUGGED_DESCRIPTION:
+                output.append('possibly_bugged')
+        if res_json['state'] == 'IN_PROGRESS':
+            output.append('login_in_progress')
+    return output
+
+STATUS_LIST = [
+    'client_connected',
+    'lcu_connected',
+    'leaverbuster_warning',
+    'new_player',
+    'login_succeed',
+    'login_in_progress',
+    'banned',
+    'possibly_bugged',
+]
+
+STATUS_FUNCTIONS = [
+    is_client_connected,
+    is_lcu_connected,
+    is_leaverbuster_warning,
+    check_login_session,
+]
 
 def get_status(connection):
     ''' Returns the status of league client '''
     status = []
-    for key, func in STATUS_MAPPING.items():
-        if func(connection):
-            status.append(key)
+    for func in STATUS_FUNCTIONS:
+        status += func(connection, status)
     return status
 
 def display_status(stdscr, status):
     ''' Displays the status in the screen '''
-    for key in STATUS_MAPPING:
-        stdscr.addstr('{}: {}\n'.format(key.title(), key in status))
+    for key in STATUS_LIST:
+        title = key.replace('_', ' ').capitalize()
+        stdscr.addstr('{}: {}\n'.format(title, key in status))

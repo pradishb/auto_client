4f23bbb50dabfb97f47ccc8245157a72ec09a1b1
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-09-08 14:56:39 +0545

    Adds main.bat

--- auto_client/main.py
+++ auto_client/main.py
@@ -3,17 +3,27 @@ import curses
 import time
 
 from efficiency.efficiency import Efficiency
+from connection.connection import Connection, LeagueConnectionException
 
 def main(stdscr):
     ''' Main function of the program '''
     efficiency = Efficiency()
+    connection = Connection()
     while True:
         start_time = time.time()
+        client_conenction = "Connected"
         stdscr.clear()
         efficiency.change()
+        try:
+            lcu_status = connection.request("/lol-service-status/v1/lcu-status", "get")
+        except LeagueConnectionException:
+            client_conenction = "Not connected"
+            lcu_status = "Not connected"
         process_time = time.time() - start_time
         stdscr.addstr('Efficiency: {:.2f} %\n'.format(efficiency.get_efficiency()))
         stdscr.addstr('Process time: {:.5f} s\n'.format(process_time))
+        stdscr.addstr('Client connection: {}\n'.format(client_conenction))
+        stdscr.addstr('Lcu status: {}\n'.format(lcu_status))
         stdscr.refresh()
         time.sleep(1)
 
